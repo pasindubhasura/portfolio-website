740d083e4c73bcb8a5daf77ddb099d37116ebf78
pasindubhasura <srmpasindubhasura@gmail.com>
2022-06-23 11:25:48 +0530

    create data file

---
+++ src/data.js
@@ -0,0 +1,44 @@
+import react from "./images/react.png";
+import node from './images/node.png';
+import express from './images/express.png';
+import mongodb from './images/mongodb.png';
+import html from './images/html.png';
+import css from './images/css.png';
+import js from './images/js.png';
+import redux from './images/redux.png';
+
+const technologies = [
+  {
+    "name": "ReactJs",
+    "image": react
+  },
+  {
+    "name": "NodeJs",
+    "image": node
+  },
+  {
+    "name": "ExpressJs",
+    "image": express
+  },
+  {
+    "name": "MongoDB",
+    "image": mongodb
+  }, {
+    "name": "HTML",
+    "image": html
+  },
+  {
+    "name": "CSS",
+    "image": css
+  },
+  {
+    "name": "JavaScript",
+    "image": js
+  },
+  {
+    "name": "Redux",
+    "image": redux
+  }
+]
+
+export { technologies };
